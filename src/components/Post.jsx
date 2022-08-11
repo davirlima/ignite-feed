787fc9527b64ed06c09401a13027dc8f658ec003
Davi Lima <davi.rlima04@gmail.com>
2022-08-11 11:02:45 -0400

    Estrutura da aplicação - Componente: avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar.jsx";
 import { Comment } from "./Comment.jsx";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="http://github.com/davirlima.png"
-          />
+          <Avatar src="http://github.com/davirlima.png" />
           <div>
             <strong>Davi Lima</strong>
             <span>Developer Student</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,25 @@
-import styles from './Sidebar.module.css';
-import { PencilLine} from "phosphor-react"
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
-export function Sidebar(){
-    return(
-        <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"/>
-            <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/davirlima.png" />
-                <strong>Davi Lima</strong>
-                <span>Developer Student</span>
-            </div>
-            <footer>
-                <a href="#"><PencilLine size={20}/>Edite seu perfil</a>
-            </footer>
-        </aside>
-    );
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/davirlima.png" />
+        <strong>Davi Lima</strong>
+        <span>Developer Student</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edite seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
